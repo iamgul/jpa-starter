12f53d2322c8490b2c114605146d3738040b0edb
Gul Mohammed <gul@bytemark.co>
2024-04-30 19:13:38 +0545

    jpa-starter

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,10 @@
+
+N
+src/main/java/io/gul/Main.java,3/6/36ce8102abf7295b07fb18a674854818cac109a1
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+R
+"src/main/java/io/gul/Employee.java,d/0/d0c07cce8840c107d08fe3ef74f3d4653da9f5e5
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/main/java/io/gul/Main.java
@@ -0,0 +1,31 @@
+package io.gul;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.setId(1);
+		e.setName("Gul");
+
+		Employee e1 = new Employee();
+		e1.setId(2);
+		e1.setName("Gul");
+
+		Employee e2 = new Employee();
+		e2.setId(3);
+		e2.setName("Gul");
+
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpa-starter");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction transaction = entityManager.getTransaction();
+		transaction.begin();
+		entityManager.persist(e);
+		entityManager.persist(e1);
+		entityManager.persist(e2);
+		transaction.commit();
+	}
+}
